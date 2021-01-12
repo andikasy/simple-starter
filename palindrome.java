9cbc8d6e368af703ed3d8659af8b2a1863508eb8
andikasy <andikasupriyana@gmail.com>
2021-01-12 22:29:25 +0700

    Create palindrome.java

---
+++ palindrome.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main (String[] args) {
+        checkPalindrome("level");
+    }
+
+    public static void checkPalindrome(String word) {
+        String anotherWord = "";
+        for (int i = word.length() - 1; i >= 0; i--) {
+            char wordChar = word.charAt(i);
+            anotherWord += String.valueOf(wordChar);
+        }
+        if (word.equals(anotherWord)) {
+            System.out.println("This " + word + " is a palindrome");
+        } else {
+            System.out.println("This " + word + " is not a palindrome");
+        }
+}
